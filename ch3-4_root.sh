32ebcc06186609818a6089d1aeae077f0911c3bb
Michele Bucca <michele.bucca@gmail.com>
2018-10-02 19:15:54 +0200

    Fix bugs in ch3-4_root.sh

--- ch3-4_root.sh
+++ ch3-4_root.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # Reading Config.txt
 source ./config.txt
@@ -6,20 +7,25 @@ source ./config.txt
 # Chapter 3
 
 # Set LFS variable for root in the .bashrc and reload it
-cat >> /root/.bashrc < EOF
+cat >> /root/.bashrc << EOF
 export LFS=$LFS
 EOF
 source /root/.bashrc
 
 # mount $LFS partition if not already (it must be in /etc/fstab)
+if [ ! -d $LFS ]; then mkdir $LFS; fi
 mount -a
 
 # Create world writable directory...
-mkdir -v $LFS/sources
+mkdir -vp $LFS/sources
 chmod -v a+wt $LFS/sources
 
 # ...and download sources inside it
+if [ ! -e $LFS/sources/download.done ]; then
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+touch $LFS/sources/download.done
+fi
+if [ ! -e $LFS/sources/md5sums ]; then cp md5sums $LFS/sources/; fi 
 pushd $LFS/sources
 md5sum -c md5sums
 popd
@@ -27,12 +33,13 @@ popd
 # Chapter 4
 
 # Creating the $LFS/tools directory
-mkdir -v $LFS/tools
-ln -sv $LFS/tools /
+if [ ! -e $LFS/tools ]; then mkdir -v $LFS/tools; fi
+ln -sfv $LFS/tools /
 
 # Adding the LFS user. PASSWORD is "lfs"
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 cat << EOF | passwd lfs
+lfs
 lfs
 EOF
